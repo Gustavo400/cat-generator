04edfefaff7d203e47f465b48a6344ebd5ca13ef
Gustavo Rios <43116946+Gustavo400@users.noreply.github.com>
2024-06-13 15:52:29 -0400

    added a text color theme

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,10 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">First step toward Kitty Central!</Text>
+      {/*Using styles from tailwind*/}
+      <Text className="text-2xl text-title">
+        First step toward Kitty Central!
+      </Text>
       <StatusBar style="auto" />
       <Link href="/kitty" className="text-blue-600">
         Let's go see kitties!
